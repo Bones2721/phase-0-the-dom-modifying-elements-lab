0af4e9d1dca5f1778196d8f2212d763a2966a8f5
Bones2721 <cwills@DESKTOP-9AD5DVS.localdomain>
2023-03-23 18:57:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+let newMain = document.getElementById("main");
+newMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Ahmaud is the champion";
+document.body.append(newHeader);
+
+
+
+// let newHeader = `<h1 id="victory">${yourName} is the champion</h1>`;
+
+// function addHeader(yourName){
+//     let body = document.body
+//     document.body.innerHTML = newHeader;
+// }
+//
+// addHeader();
